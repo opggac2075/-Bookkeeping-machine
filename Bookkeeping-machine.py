9d178ff5bf1515daa88198ba26a87cebf66d5ca7
skyoui <zedgod33333@gmail.com>
2019-10-07 13:34:22 +0800

    優化程式碼,改成更好的寫法

--- Bookkeeping-machine.py
+++ Bookkeeping-machine.py
@@ -9,8 +9,7 @@ while True:
         break
     price = input('請輸入購買的商品價格:')
     price = int(price)
-    d = str(price)                          #因為int在write的時候不能夠被連接(TypeError: can only concatenate str (not "int") to str)，用型別轉換轉成str之後再存成另一個變數加入清單，就可以符合write的條件了
-    products.append([name, price, d])       #以上功能為輸入商品及價格，加入總清單
+    products.append([name, price])       #以上功能為輸入商品及價格，加入總清單
     sum += price                            #將所有的花費及項目做總結
 print('這是你的商品總表:', products)         #以下功能為將帳務資訊顯示
 for p in products:
@@ -18,8 +17,8 @@ for p in products:
 print('你的商品總共有', count-1, '項')
 print('你總共花費的金額:$', sum)
                                                 #以下功能為將輸入的商品項目及價格以表格的方式存入電腦中
-with open('Accounttable.csv', 'w') as f:   
+with open('Account_table.csv', 'w') as f:   
     for p in products:
-        f.write(p[0] + ',' + p[2] + '\n' )
+        f.write(p[0] + ',' + str(p[1]) + '\n' )  #因為int在write的時候不能夠被連接(TypeError: can only concatenate str (not "int") to str)，用型別轉換轉成str之後再存成另一個變數加入清單，就可以符合write的條件了
 
  
